abe0228e51f86e4b9580212b542d92fdf1617666
fox1234567899 <sefidrubah@gmail.com>
2026-02-25 02:01:18 -0800

    add another thing in my url

--- APAME/urls.py
+++ APAME/urls.py
@@ -36,5 +36,4 @@ urlpatterns = [
     path("token/",views.token_login, name='token_login'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-if settings.DEBUG:
-    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
