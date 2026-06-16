de27710dde6fc0a621774ea7995fb064cdde4654
fox1234567899 <sefidrubah@gmail.com>
2026-06-16 05:45:15 -0700

    change setting 2

--- APAME/settings.py
+++ APAME/settings.py
@@ -198,7 +198,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK ={
     'DEFAULT_AUTHENTICATION_CLASSES':(
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        
     )
 }
 
